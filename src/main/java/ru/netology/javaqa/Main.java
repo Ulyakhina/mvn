f4ae9fc369881c220b904b692aabd40acb9ef98f
Anastasiya Ulyakhina <ulyakhina.a@gmail.com>
2023-03-06 09:59:48 +0300

    first commit

---
+++ src/main/java/ru/netology/javaqa/Main.java
@@ -0,0 +1,10 @@
+package ru.netology.javaqa;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        int expected = 150;
+        System.out.println("Hello world!");
+    }
+}
